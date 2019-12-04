35c7a940dfabb7ff2d2c2b5e3677f98f3de60d7c
Jason Chafin <jchafin@ucsc.edu>
2019-12-04 15:04:59 -0800

    carving ...

--- source/assets/js/toggle.js
+++ source/assets/js/toggle.js
@@ -1,3 +1,7 @@
 $(".omnitoggle").on("click", function() {
     $(".omnitoggle").parent().toggleClass('active');
 });
+
+$(".nav-toggle").on("click", function() {
+    $(".nav-toggle").toggleClass('active');
+});
